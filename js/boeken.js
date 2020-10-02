bd1b3f75f8b48324c4cbe69667efce1efdf23a31
Joeyvdkuijl <55228974+Joeyvdkuijl@users.noreply.github.com>
2020-10-02 13:40:52 +0200

    Add files via upload

---
+++ js/boeken.js
@@ -0,0 +1,23 @@
+const uitvoer = document.getElementById('boeken');
+const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && xhr.status ==200){
+        let resultaat = JSON.parse(xhr.responseText);
+        boeken.data = resultaat;
+        boeken.uitvoeren();
+    }
+}
+xhr.open('GET', 'boek.json', true);
+xhr.send();
+
+const boeken = {
+    
+    uitvoeren() {
+        let html = "";
+        this.data.forEach( boek => {
+            html += `<h3>${boek.titel}</h3>`;
+        });
+        uitvoer.innerHTML = html
+    }
+}
