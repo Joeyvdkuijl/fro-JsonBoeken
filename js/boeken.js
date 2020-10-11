3bf94fd98efd31533747c6d00336fdc9d669325c
Joeyvdkuijl <55228974+Joeyvdkuijl@users.noreply.github.com>
2020-10-11 12:51:23 +0200

    Add files via upload

--- js/boeken.js
+++ js/boeken.js
@@ -15,8 +15,32 @@ const boeken = {
     
     uitvoeren() {
         let html = "";
+
         this.data.forEach( boek => {
-            html += `<h3>${boek.titel}</h3>`;
+
+            let completeTitel = " ";
+            if (boek.voortitel) {
+                completeTitel += boek.voortitel + " ";
+            }
+            completeTitel += boek.titel;
+
+            let auteurs = "";
+            boek.auteurs.forEach(schrijver => {
+                let tv = schrijver.tussenvoegsel ? schrijver.tussenvoegsel+" " : "";
+                let separator = " "
+                auteurs += schrijver.voornaam + " " + tv + schrijver.achternaam + separator;
+            })
+
+            html += `<section class="boek">`;
+            html += `<img class="boek__cover" src="${boek.cover}" alt="${completeTitel}">`;
+            html += `<h3 class="boek__kopje">${completeTitel}</h3>`;
+            html += `<p class="boek__auteurs">${auteurs}</p>`;
+            html += `<span class="boek__uitgave"> ${boek.uitgave}</span>`;
+            html += `<span class="boek__ean"> ean ${boek.ean}</span>`;
+            html += `<span class="boek__paginas"> ${boek.paginas} pagina's. </span>`;
+            html += `<span class="boek__taal"> ${boek.taal}</span>`;
+            html += `<div class="boek__prijs">&euro;${boek.prijs}</div>`;
+            html += `</section>`;
         });
         uitvoer.innerHTML = html
     }
